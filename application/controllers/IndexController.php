53463feff9d8fc0a6a32a14669375ae0427443cf
Eric Leclerc <eric.leclerc@gmail.com>
2009-07-15 20:54:49 -0400

    applied theme by styleshout.com

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -1,6 +1,4 @@
 <?php
-//require_once 'BaseMessage.php';
-//require_once 'Message.php';
 
 class IndexController extends Zend_Controller_Action
 {
@@ -12,26 +10,6 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        $db = $this->getInvokeArg('bootstrap')->getResource('db');
-
-        $blogSelect = $db->select()
-            ->from('blog', 'url')
-            ->join('blog_post', 'blog.id=blog_post.blog_id', array('title', 'content', 'posted_on', 'oriUrl' => 'url'))
-            ->where('live = 1')
-            ->order('posted_on DESC')
-            ->limit(30);
-            
-        $this->view->blogposts = $db->fetchAll($blogSelect);
-
-
-        $twitterSelect = $db->select()
-            ->from('twitter', 'screen_name')
-            ->join('twitter_post', 'twitter.id=twitter_post.twitter_id', array('content', 'posted_on'))
-            ->where('live = 1')
-            ->order('posted_on DESC')
-            ->limit(30);
-
-        $this->view->tweets = $db->fetchAll($twitterSelect);
+        // Static page for now
     }
 }
-
